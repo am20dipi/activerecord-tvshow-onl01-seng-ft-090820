898685f0b00e49f8bfe3b67c2c97a9a8c12eac94
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-13 15:27:59 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,9 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end 
   
-  def self.most_popular_show 
+  def self.most_popular_show
+    most_popular_show = self.highest_rating
+    most_popular_show
   end
   
   def self.lowest_rating
