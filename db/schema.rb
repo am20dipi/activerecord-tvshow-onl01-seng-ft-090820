495170f12d57c75c687635dbc18c7f7e4d021597
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-13 15:20:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
-# Could not dump table "shows" because of following StandardError
-#   Unknown type 'striing' for column 'season'
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "network"
+    t.string "day"
+    t.integer "rating"
+  end
 
 end
